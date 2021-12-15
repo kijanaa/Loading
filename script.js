622e402befa68344a29aa03a00f307237af1f9f5
kijanaa <irene.yanapouth@gmail.com>
2021-12-14 18:07:48 -0800

    loading

---
+++ script.js
@@ -0,0 +1,14 @@
+const loadText = document.querySelector('')
+const bg = document.querySelector()
+
+
+let load = 0
+
+let int = setInterval(blurring, 30)
+
+
+function blurring(){
+    load++
+
+    console.log(load)
+}
